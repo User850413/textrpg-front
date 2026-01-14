0daa351c53a611a57daae89dd9d8f0c0b392704e
s000h99@winitech.com <S000h99@winitech.com>
2026-01-14 17:57:50 +0900

    260114 :rotating_light: : 오타 수정

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export function useForm<T extends Record<string, any>(initialState: T){
+export function useForm<T extends Record<string, any>>(initialState: T){
     const [form, setForm] = useState<T>(initialState);
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
